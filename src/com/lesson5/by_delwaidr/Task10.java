c6736bd56957db8bb166de1a817a406814f42cfa
DELWAIDR <deladred.aledriand@gmail.com>
2018-07-29 19:01:53 +0300

    Заполнене массива типа  String днями недели и вывод 6-го желемента на консоль

---
+++ src/com/lesson5/by_delwaidr/Task10.java
@@ -0,0 +1,9 @@
+package com.lesson5.by_delwaidr;
+
+public class Task10 {
+    public static void main(String[] args) {
+        String daysweek[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sut", "Sun"};
+        System.out.println(daysweek[6]);
+
+    }
+}
