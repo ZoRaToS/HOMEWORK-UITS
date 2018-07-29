9ed5ef43668247a93ce2dfd523abc9289a701956
DELWAIDR <deladred.aledriand@gmail.com>
2018-07-29 19:05:49 +0300

    Заполнене массива типа  double числами и вывод первого желемента на консоль

---
+++ src/com/lesson5/by_delwaidr/Task11.java
@@ -0,0 +1,8 @@
+package com.lesson5.by_delwaidr;
+
+public class Task11 {
+    public static void main(String[] args) {
+        double d [] = {4.3,5.7,1.2,6.9};
+        System.out.println(d[0]);
+    }
+}
