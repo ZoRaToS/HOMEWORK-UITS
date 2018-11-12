4437e4841c64ba94078f96b27e9427e48aa7842e
DELWAIDER <dovakin.izotov1994@gmail.com>
2018-11-12 22:19:03 +0200

    lesson 3 task 4

---
+++ src/com/lesson3/by_delwaidr/Task4.java
@@ -0,0 +1,31 @@
+package com.lesson3.by_delwaidr;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        sumOFnumbers();
+    }
+
+    public static void sumOFnumbers() {
+        int a = 0, b = 0;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите число а");
+        if (scn.hasNextInt()) {
+            a = scn.nextInt();
+            System.out.println("Введите число b");
+
+
+        } else {
+            System.out.println("Вы ввели не целое число");
+        }
+        if (scn.hasNextInt()) {
+            b = scn.nextInt();
+        } else {
+            System.out.println("Вы ввели не целое число");
+
+        }
+        System.out.println("Сумма равна: " + (a + b));
+
+    }
+}
